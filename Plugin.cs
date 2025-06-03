58053f1c039798cb8f81627d0a33ee6ae7a5d375
Doug <doug.stalker@gmail.com>
2025-06-04 00:09:11 +1000

    Working, despite all the warnings

--- Plugin.cs
+++ Plugin.cs
@@ -1,47 +1,42 @@
-﻿using System;
-//using System.Reflection;
-using BepInEx;
+﻿using BepInEx;
 using BepInEx.Logging;
-//using BepInEx.IL2CPP;
-using BepInEx.Unity.IL2CPP; // this causes huge numbers of erros because it wants 
+using BepInEx.Unity.IL2CPP; // this causes huge numbers of warnings because it wants System.Runtime package 6.0.0.0 instead of a verion that exists
 using HarmonyLib;
-using Awaken.TG.Main.Locations.Actions.Lockpicking;
 using Awaken.TG.Main.Locations.Actions;
+using Awaken.TG.Main.Heroes.Interactions;
+using Awaken.TG.Main.Heroes;
+
 
 namespace EasyLockpicking
 {
-    [BepInPlugin("Nep.EasyLockpicking", "EasyLockpicking", "1.0.0")]
-    public class Plugin : BasePlugin
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
     {
         private static Harmony _harmony;
         internal static ManualLogSource MyLog; //So DebugLog can be called as a static function, once the Load() method sets up this variable
         public override void Load()
         {
+            Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loading.");
             // Plugin startup logic
+            System.Threading.Thread.Sleep(10000);
             MyLog = base.Log;
             _harmony = Harmony.CreateAndPatchAll(typeof(Plugin));
-            Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded! HELLO WORLD!");
+            Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded. HELLO WORLD!");
         }
 
         public static void DebugLog(string msg)
         {
             MyLog.LogInfo(msg);
         }
-
-        [HarmonyPatch(typeof(LockAction), "OnFullyInitialized")]
+ 
+        [HarmonyPatch(typeof(LockAction), "OnStart")] //Triggers when a 
         [HarmonyPostfix]
-        public static float OnFullyInitializedPostfix(LockTolerance __instance, ref float __result)
+        public static void LockAction_OnStart_Postfix(LockAction __instance, Hero hero, IInteractableWithHero interactable) 
         {
-            Plugin.DebugLog($"LockTolerance OnFullyInitialized Postfix");
-            return __result;
-        }
+            Plugin.DebugLog($"LockAction OnStart Postfix");
+            __instance.DecreaseDifficulty(100);
+            //__instance.Unlock(false)
 
-        [HarmonyPatch(typeof(LockTolerance), "angle")]
-        [HarmonyPostfix]
-        public static float LockTolerancePostfix(LockTolerance __instance, ref float __result)
-        {
-            Plugin.DebugLog($"LockTolerance.angle returns: {__result}");
-            return __result;
         }
 
 
