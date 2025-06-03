d9802a5ec9d1e605d1ddfa5d3bb5b9b079803268
Doug <doug.stalker@gmail.com>
2025-06-03 17:53:41 +1000

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Core.Logging.Interpolation;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace EasyLockpicking
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BasePlugin
+    {
+        public override void Load()
+        {
+            // Plugin startup logic
+            Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded! HELLO WORLD!");
+        }
+    }
+}
---
+++ install.bat
@@ -0,0 +1,1 @@
+copy "D:\TGFoA\EasyLockpicking\bin\Debug\netstandard2.1\NepEasyLockpicking.dll"  "C:\Games\Steam\steamapps\common\Tainted Grail FoA\BepInEx\plugins\"
